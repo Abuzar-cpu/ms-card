dd368905fedc68b1414efdce0b04351f8f4cf7de
Abuzar Karimzada <kerimovabuzer25@gmail.com>
2024-03-17 19:56:02 +0400

    initial commit

---
+++ src/main/kotlin/az/vtb/mscard/MsCardApplication.kt
@@ -0,0 +1,11 @@
+package az.vtb.mscard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsCardApplication
+
+fun main(args: Array<String>) {
+    runApplication<MsCardApplication>(*args)
+}
---
+++ src/test/kotlin/az/vtb/mscard/MsCardApplicationTests.kt
@@ -0,0 +1,13 @@
+package az.vtb.mscard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsCardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
